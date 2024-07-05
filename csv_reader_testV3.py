3aad86958d4c184675008633cbd1f6a61083fac5
sdurham24 <154894263+sdurham24@users.noreply.github.com>
2024-07-05 13:44:22 +0000

    Create folders, alter csv_exist function

--- csv_reader_testV3.py
+++ csv_reader_testV3.py
@@ -13,18 +13,22 @@ logging.basicConfig(filename="process_info.log", format='%(levelname)s:%(message
 CMP_CODES = []
 INVALID_CODES = []
 PDF_NAMES = []
-CSV_NAMES = ['shipping_export.csv', 'invalid_codes.csv']
+CSV_FILE_CHECK = ['shipping_export.csv', 'invalid_codes.csv']
+CSV_FILE_EXIST = []
 TEMPLATE = 'MSDS Template.docx'
 
 def csv_exist():
-    for name in CSV_NAMES:
-        try:
-            if not os.path.exists(name):
-                raise FileNotFoundError(f'The file "{name}" does not exist.')
-        except FileNotFoundError as e:
-            print(e)
-            logging.error(e)
-            raise
+    try:
+        for name in CSV_FILE_CHECK:
+            if os.path.exists(name):
+                CSV_FILE_EXIST.append(name)
+        if not CSV_FILE_EXIST:
+            raise FileNotFoundError(f'There are no valid csv files in this folder.')
+    except FileNotFoundError as e:
+        print(e)
+        logging.error(e)
+        raise
+
 
 def template_exist():
     try:
@@ -35,5 +39,12 @@ def template_exist():
         logging.error(e)
         raise
 
+def create_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
 csv_exist()
 template_exist()
+print(CSV_FILE_EXIST)
+create_folder('MSDS pdfs')
+create_folder('MSDS raw files') 
